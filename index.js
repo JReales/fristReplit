8f9eb9a0790177634839e4d633e2aa7e974ac512
Julián David Reales de la Ossa <73629180+JReales@users.noreply.github.com>
2022-04-02 16:44:21 +0000

    New class add

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { User } from "./User.js"
+import { Product } from "./Product.js"
 
 const name = new User('Julian', 'Reales')
 
 console.log(name)
+console.log(new Product("Laptop", "2000"))
